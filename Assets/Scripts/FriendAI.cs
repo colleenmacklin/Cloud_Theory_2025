79ab2f9f94e865a25aecb67511ffb170f32b926d
Colleen Macklin <colleenmacklin@gmail.com>
2026-01-13 12:49:09 -0500

    new main scene

---
+++ Assets/Scripts/FriendAI.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.U2D;
+using LLMUnity;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using UnityEditor.Build.Reporting;
+public class FriendAI : MonoBehaviour
+{
+
+    public LLM llm;
+    public LLMCharacter llmCharacter;
+    public string Prompt = "...";
+    public string chosenCloud;
+    public TextMeshProUGUI ChatText;    public int numWords;
+
+    private void OnEnable()
+    {
+      Actions.ChooseCloud += LookAtCloud;  
+    }
+
+        private void OnDisable()
+    {
+      Actions.ChooseCloud -= LookAtCloud;  
+    }
+
+    public void LookAtCloud(string s)
+    {
+        //cloudHistory.Add(s); //need to reference CloudManager...
+        //Debug.Log("cloudhistory: "+cloudHistory.Last());
+        chosenCloud = s;
+    }
+
+}
